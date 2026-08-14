b6444ae2a7505eae71c1a53529a62657386c219c
Andres Laverde <andres.laverde@protonmail.com>
2026-08-13 20:04:35 -0700

    update: refactor project stucture

--- models/note.js
+++ models/note.js
@@ -1,28 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery',false)
-
-const url = process.env.MONGODB_URI
-
-console.log('Connecting to', url)
-
-mongoose.connect(url, { family: 4 })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('Error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
-// Overrite toJSON schema method to avoid returning _id as object.
+
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
